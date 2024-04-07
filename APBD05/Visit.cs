04113d7027a72c7752e6417f9291bca41356080f
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-07 23:03:34 +0200

    add Visit class

---
+++ APBD05/Visit.cs
@@ -0,0 +1,9 @@
+namespace APBD05;
+
+public class Visit
+{
+    public DateTime Date { get; set; }
+    public Animal Animal { get; set; }
+    public string description { get; set; }
+    public double price { get; set; }
+}
